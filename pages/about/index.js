e601320f5561e91c02d40dbed9bcade7de0574ff
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-01-28 11:39:20 +0530

    Bug Fix

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,95 +1,290 @@
+import React, { useState } from "react";
 // icons
 import {
   FaHtml5,
   FaCss3,
   FaJs,
   FaReact,
-  FaWordpress,
-  FaFigma,
+  FaAndroid,
+  FaJava,
+  FaPython,
 } from "react-icons/fa";
 
 import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
+  SiAndroidstudio,
+  SiUnity,
+  SiBlender,
+  SiCsharp,
+  SiDjango,
+  SiKalilinux,
+  SiWireshark,
+  SiVmware,
+  SiWearos,
+  SiArduino,
 } from "react-icons/si";
 
+import { RiAndroidLine, RiFlutterLine } from "react-icons/ri";
 
 //  data
 const aboutData = [
   {
-    title: 'skills',
+    title: "skills",
     info: [
       {
-        title: 'Web Development',
+        title: "Web Development",
+        icons: [<FaHtml5 key="html" />, <FaCss3 key="css" />, <FaJs key="js" />, <FaReact key="react" />, <SiDjango key="django" />],
+      },
+      {
+        title: "Android Development",
         icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <FaAndroid key="android" />,
+          <SiWearos key="wearos" />,
+          <FaJava key="java" />,
+          <RiFlutterLine key="flutter" />,
+          <SiAndroidstudio key="androidstudio" />,
         ],
       },
       {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        title: "Game Development",
+        icons: [<SiCsharp key="csharp" />, <SiBlender key="blender" />, <SiUnity key="unity" />],
+      },
+      {
+        title: "Ethical Hacking",
+        icons: [<SiKalilinux key="kalilinux" />, <SiWireshark key="wireshark" />, <SiVmware key="vmware" />],
+      },
+      {
+        title: "IOT",
+        icons: [<FaPython key="python" />, <SiArduino key="arduino" />],
+      },
+    ],
+  },
+  // {
+  //   title: "awards",
+  //   info: [
+  //     {
+  //       title: "Webby Awards - Honoree",
+  //       stage: "2011 - 2012",
+  //     },
+  //     {
+  //       title: "Adobe Design Achievement Awards - Finalist",
+  //       stage: "2009 - 2010",
+  //     },
+  //   ],
+  // },
+  {
+    title: "experience",
+    info: [
+      {
+        title: "Junior Software Developer - Tvast Company",
+        stage: "2023 - Present",
+      },
+      {
+        title: "Junior Software Developer - Rekonsile Company",
+        stage: "2023 - Present",
+      },
+      {
+        title: "Youth for Seva - Volunteer + Intern",
+        stage: "2021 - Present",
+      },
+      {
+        title: "National Servic Scheme - Volunteer",
+        stage: "2021 - Present",
       },
     ],
   },
   {
-    title: 'awards',
+    title: "Certifications",
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: "Ethical Hacking - NPTEL",
+        stage: "2021",
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: "UI/UX - Great Learning ",
+        stage: "2021",
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        title: "No-Code AI Model - Navan.ai ",
+        stage: "2021",
       },
       {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        title: "Python - NPTEL/ Data Flair",
+        stage: "2022",
       },
       {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
+        title: "Java - KSIT",
+        stage: "2022",
       },
       {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: "Chess - VTU",
+        stage: "2022",
       },
     ],
   },
 ];
 
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+import CountUp from "react-countup";
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="h-full bg-primary/30 py-36 text-center xl:text-left overflow-y-auto">
+      <Circles />
+
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden md:hidden xl:flex absolute bottom-0 -left-[230px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div
+        className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6"
+        style={{ marginRight: 150 }}
+      >
+        <div className="flex-1 flex flex-col justify-center">
+          <motion.h2
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            Versatile Tech <span className="text-accent"> Professional </span>
+            Fusing Innovation
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 "
+            style={{ textAlign: "justify" }}
+          >
+            With expertise in languages like HTML, CSS, JavaScript, React,
+            Flutter and Java, I bring over 2 years of experience as a Junior
+            Software Developer. Committed to continuous learning, I hold 15+
+            certifications, reflecting my passion for innovation and excellence
+            in the ever-evolving tech landscape.
+          </motion.p>
+          <motion.div
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-0"
+          >
+            <div className="flex flex-1 xl:gap-x-9">
+              <div
+                className="relative flex-1 after:w-[1px] 
+            after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0"
+              >
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent">
+                  <CountUp start={-2} end={2} duration={5} /> +
+                </div>
+                <div
+                  className="text-xs uppercase tracking-[1px] leading-[1.4] 
+              max-w-[100px]"
+                >
+                  {" "}
+                  Years of experience{" "}
+                </div>
+              </div>
+              <div
+                className="relative flex-1 after:w-[1px] 
+            after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0"
+              >
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent">
+                  <CountUp start={-2} end={5} duration={5} /> +
+                </div>
+                <div
+                  className="text-xs uppercase tracking-[1px] leading-[1.4] 
+              max-w-[100px]"
+                >
+                  {" "}
+                  Projects{" "}
+                </div>
+              </div>
+              <div
+                className="relative flex-1 after:w-[1px] 
+            after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0"
+              >
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent">
+                  <CountUp start={-2} end={15} duration={5} /> +
+                </div>
+                <div
+                  className="text-xs uppercase tracking-[1px] leading-[1.4] 
+              max-w-[100px]"
+                >
+                  {" "}
+                  Certifications{" "}
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px] overflow-y-auto"
+        >
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((items, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={` ${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 
+                  after:h-[2px] after:bg-white after:absolute after:-bottom-1 
+                  after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {items.title}
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4
+          items-center xl:items-start"
+          >
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row
+                max-w-max gap-x-2 items-center text-white/60"
+                  style={{ maxHeight: "300px", overflowY: "auto" }}
+                >
+                  <div className="font-light md-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex"> - </div>
+                  <div> {item.stage} </div>
+                  <div className="flex gap-x-4">
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className="text-2xl text-white" key={index}>{icon}</div>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
