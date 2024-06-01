bd99321d736785adc2ec7e44f7919125d4f7b495
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-02 01:36:32 +0530

    Projects Update

---
+++ pages/projects/index.js
@@ -0,0 +1,58 @@
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+const Work = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-x-8">
+          <div
+            className="text-center flex flex-col mb-4
+          xl:mb-0"
+          >
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-12 xl:text-5xl"
+            >
+              My <span className="text-accent">&nbsp;Projects.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mx-auto text-sm lg:mx-0"
+            >
+              Throughout my career as a Junior Software Developer, I have worked
+              on a diverse range of projects that demonstrate my technical
+              skills, creativity, and problem-solving abilities. Each project
+              has provided me with valuable experience in different aspects of
+              software development, including frontend and backend development,
+              mobile app creation, and cloud integration.
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[100vw]"
+          >
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default Work;
